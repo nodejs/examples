654be779d380ad18b4cbb51607dde289e95a81fe
Tierney Cyren <hello@bnb.im>
2020-04-02 16:55:20 -0400

    test(countEntriesInDirectory): make the test async

--- cli/yargs/countEntriesInDirectory/lib/countEntries.test.js
+++ cli/yargs/countEntriesInDirectory/lib/countEntries.test.js
@@ -1,13 +1,10 @@
 const countEntries = require('./countEntries')
 
 describe('test `countFiles()` successful runs', () => {
-  test('Test entries in cli/yargs/countEntriesInDirectory', () => {
-    async function getEntries () {
-      const directory = __dirname // this will be `lib`
-      const entries = await countEntries(directory)
-      expect(entries).toBe(2)
-    }
-
-    getEntries()
+  test('Test entries in cli/yargs/countEntriesInDirectory', async () => {
+    const directory = __dirname // this will be `lib`
+    const entries = await countEntries(directory)
+    
+    expect(entries).toBe(4)
   })
 })
