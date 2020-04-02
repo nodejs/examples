710684eb69d89385e8f1f2e2f3dba082718ed0e2
Tierney Cyren <hello@bnb.im>
2020-04-02 17:01:29 -0400

    chore(countEntriesInDirectory): add blank line at end of countEntries.test.js

--- cli/yargs/countEntriesInDirectory/lib/countEntries.test.js
+++ cli/yargs/countEntriesInDirectory/lib/countEntries.test.js
@@ -4,7 +4,7 @@ describe('test `countFiles()` successful runs', () => {
   test('Test entries in cli/yargs/countEntriesInDirectory', async () => {
     const directory = __dirname // this will be `lib`
     const entries = await countEntries(directory)
-    
+
     expect(entries).toBe(4)
   })
 })
