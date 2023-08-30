1e2c731a720ca4e10413e058e35c1e4768a8e94a
Oscar Sanchez <oscar.dri@gmail.com>
2023-08-30 11:34:51 +0200

    Add Lambda compatibility

--- servers/express/api-with-express-and-handlebars/index.js
+++ servers/express/api-with-express-and-handlebars/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const serverless = require('serverless-http') // AWS Lambda requirement and Express
 const hbs = require('hbs')
 const routes = require('./routes/routes')
 const path = require('path')
@@ -9,7 +10,12 @@ app.set('view engine', hbs)
 app.use('/', routes)
 app.use(express.static(path.join(__dirname, '/public')))
 
-app.listen(PORT, () => {
+// We require two executions, one for local development and another for the Lambda execution.
+if (process.env.ENVIRONMENT === 'production') {
+  module.exports.handler = serverless(app);
+} else {
+  app.listen(PORT, () => {
   console.log(`app is running on PORT ${PORT}`)
-})
-module.exports = app
+  })
+  module.exports = app
+}
